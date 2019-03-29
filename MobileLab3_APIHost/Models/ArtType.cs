9f7098cf3d48b7cf02a27e6b7fd8936232f59076
KenLDubois <kenldubois@gmail.com>
2019-03-29 10:32:32 -0400

    API host complete: - controllers - ArtType data contract

--- MobileLab3_APIHost/Models/ArtType.cs
+++ MobileLab3_APIHost/Models/ArtType.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace MobileLab3_APIHost.Models
 {
+    [DataContract(IsReference = true)]
     public class ArtType
     {
         public ArtType()
         {
             this.Artworks = new HashSet<Artwork>();
         }
+        [DataMember]
         public int ID { get; set; }
 
+        [DataMember]
         [Display(Name = "Art Type")]
         [Required(ErrorMessage = "You cannot leave the art type name black.")]
         [StringLength(25, ErrorMessage = "Art type cannot be more than 25 characters long.")]
