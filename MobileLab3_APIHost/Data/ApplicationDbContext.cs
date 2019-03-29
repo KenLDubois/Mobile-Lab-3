101425a587a02bdc5504914d937ee783ce6ccedb
Ken Dubois <kdubois1@ncstudents.niagaracollege.ca>
2019-03-29 09:37:16 -0400

    Add project files.

---
+++ MobileLab3_APIHost/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileLab3_APIHost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobileLab3_APIHost.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<ArtType> ArtTypes { get; set; }
+        public DbSet<Artwork> Artworks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artwork>()
+                .HasIndex(a => new { a.ArtTypeID, a.Name })
+                .IsUnique();
+        }
+
+
+    }
+}
